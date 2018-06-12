7a0f929da837b5630f41bfa63ad030706fa18a1d
Shridevi58 <35360276+Shridevi58@users.noreply.github.com>
2018-06-12 17:37:17 +0530

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST['button_pressed']))
+{
+    $to      = 'nobody@example.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com' . "\r\n" .
+        'Reply-To: webmaster@example.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    mail($to, $subject, $message, $headers);
+
+    echo 'Email Sent.';
+}
+
+?>
